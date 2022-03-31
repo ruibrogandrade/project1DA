c2480176a546088a0e553aa2175af2b3c8da56f0
gdrp <up202007375@fe.up.pt>
2022-03-31 19:15:01 +0100

    fileReader functions

--- main.cpp
+++ main.cpp
@@ -4,5 +4,8 @@
 int main() {
     Application app;
     app.fileReaderPackages();
+    app.fileReaderTransport();
+    cout << app.getPackage().size() << endl;
+    cout << app.getTransport().size();
     return 0;
 }
